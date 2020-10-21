344a6ef0e743570aa0ef6e54e2c1b967953a16fa
majbc1999 <majbc1999@gmail.com>
2020-10-22 00:56:11 +0200

    Dodana prijava

--- spetni_umesnik.py
+++ spetni_umesnik.py
@@ -3,7 +3,7 @@ import json
 
 
 from main import Uporabnik, Tekma, Napoved, Tabela
-from orodje_za_json import dodaj_uporabnika, preveri
+from orodje_za_json import dodaj_uporabnika, preveri, ustrezno
 
 # Tukaj se potem začne spet graditi spletna stran
 
@@ -19,6 +19,10 @@ def vrni_napako():
 def vrni_napako2():
     return bottle.template("napaka2.tpl")
 
+@bottle.get("/napaka3/")
+def vrni_napako3():
+    return bottle.template("napaka3.tpl")
+
 @bottle.post("/registracija/<username>/<password>/<password2>/")
 def registracija(username, password, password2):
     username = bottle.request.forms.getunicode("ime")
@@ -33,11 +37,18 @@ def registracija(username, password, password2):
             dodaj_uporabnika(username, password)
             bottle.redirect("/uspesnaregistracija")
 
-
 @bottle.get("/uspesnaregistracija")
 def vrni_uspeh():
     return bottle.template("uspeh.tpl")
-        
+
+@bottle.post("/main/")
+def prijava():
+    username = bottle.request.forms.getunicode("username")
+    password = bottle.request.forms.getunicode("password")
+    if ustrezno(username,password):
+        return bottle.template("main.tpl", username=username)
+    else:
+        bottle.redirect("/napaka3/")
 # Program moramo na koncu še pognati
 
 bottle.run(reloader=True, debug=True)
